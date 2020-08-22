3bec10c2ab6a069c0c641c34be3dde4e2e1786ac
kkdysite <68997453+kkdysite@users.noreply.github.com>
2020-08-22 11:54:27 +0800

    123

--- initialization.js
+++ initialization.js
@@ -15,15 +15,23 @@ var notecontent='新版本极致精简，去掉了一些不常用功能，请从
 notecontent+='<div style="text-align: center;">==== 预 告 ====</div>';
 notecontent+='为了降低成本：会在8月下旬会陆续迁移部分功能到Github和CF等服务器，期间可能会有部分用户无法打开，请重启设备或者切换网络。';
 notecontent+='<div style="text-align: center;">==== 最近更新 ====</div>';
+notecontent+='[08-19 20:30]修复分享的二维码的bug<br>';
 notecontent+='[08-19 18:30]解决了一些已知问题<br>';
 notecontent+='[08-16 16:10]修复播放记录不能自动更新的bug<br>[08-13 23:30]新增资源库(天空云)<br>';
 
+var SharpUrl_dl='http://app.kkdy.website/';
+var SharpUrl_reg='http://app.kkdy.website/#page_reg';
+var SharpUrl_dl_uid='http://app.kkdy.website/?uid=UID';
+var SharpUrl_reg_uid='http://app.kkdy.website/?uid=UID#page_reg';
+var Invitation_code_prefix=9900; //邀请码前缀 ★
+
+
 var app_name={'web':'看看电影.com','local':'测试_看看电影','bak':'看看电影(备用)'};
 var app_var='<br>中文网址：看看电影.com<br>';
-app_var+='云版号：ver.2020.0819.12<br>';
+app_var+='云版号：ver.0819.7.51<br>';
 app_var+=window.location.host;
  //模板文件地址 css js地址 api
- var cdn='https://cdn.jsdelivr.net/gh/kkdysite/webapp@master/tpl/0819_12';
+ var cdn='https://cdn.jsdelivr.net/gh/kkdysite/webapp@master/tpl/0819.7.51';
  var tpl_Js_html={
 	 'web':cdn+'/tpl_Js_html/',
 	 'local':'./tpl_Js_html/',
@@ -51,7 +59,7 @@ app_var+=window.location.host;
  var localStor_name_u_portable_fav='u_portable_fav2020';
 
 //var static_file_suffix ="";
-var portable_url='http://t.kkdy.website';
+var portable_url='t.kkdy.website';
 
 //解析服务器
 var ParseServ1="https://api.fenglinys.net/?url=";
@@ -88,7 +96,6 @@ var vip_setting = {
 };
 
 
-var Invitation_code_prefix=9900; //邀请码前缀 ★
 
 
 //调用index.js
