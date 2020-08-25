5a2799d9fa405e30c9efd4a89e3f1c8f55165b90
kkdysite <68997453+kkdysite@users.noreply.github.com>
2020-08-25 22:57:21 +0800

    move

--- initialization.js
+++ initialization.js
@@ -1,23 +1,39 @@
-if (window.location.host=='10.0.0.88:81'){
-	var WL='local';
-	var cdn_min='';
-}else if(window.location.host=='kkdysite.github.io'){
-	var WL='bak';
-	static_file_suffix='';
-	var cdn_min='';
+/* 区分测试还是线上*/
+var WL='web';
+static_file_suffix='';
+var cdn_min='.min';
+if(window.location.port){
+	if (window.location.port!='80'){
+		console.log("本地port:"+window.location.port);
+		WL='local';
+		cdn_min='';
+		static_file_suffix='?'+Math.random();
+	}
 }else{
-	var WL='web';
-	static_file_suffix='';
-	var cdn_min='.min';
+	for (i = 0; i < 20; i++) {
+		console.log(i+' 自学编程，业余开发，熬夜无数，手下留情');
+	}
 }
-var notetitle='App更新';
-var notecontent='新版本极致精简，去掉了一些不常用功能，请从搜索开始使用。<br>热片推荐近期会推送更新，请耐心等待。11月之前安卓和电脑版客户端更新。<br>如您有建议和反馈，欢迎留言给我们。';
+if(window.location.host=='kkdysite.github.io'){
+	WL='bak';
+	static_file_suffix='';
+	cdn_min='';
+}
+/* 最后更新版本 */
+var laset_ver='0825.1';
+/* 首页公告 */
+var notetitle='最新说明';
+var notecontent='极致精简，去掉了一些不常用功能，请从搜索开始使用。';
+notecontent+='热片推荐近期会推送更新，请耐心等待。11月之前安卓和电脑版客户端更新。<br>如您有建议和反馈，欢迎留言给我们。';
+notecontent+='<br>最后更新: 2020.'+laset_ver+'<a href="javascript:Div_show_hide(\'MoreNote\',\'openMoreNote\',\' 详细 \')" class="openMoreNote"> 详细 </a>';
+notecontent+='<div class="hide MoreNote">';
 notecontent+='<div style="text-align: center;">==== 预 告 ====</div>';
 notecontent+='正在迁移到github，期间可能会有部分用户无法打开，请重启设备或者切换网络。';
 notecontent+='<div style="text-align: center;">==== 最近更新 ====</div>';
+notecontent+='[08-25 19:50]修复苹果设备横竖屏转回bug<br>  修复了app内的注册的bug<br>';
 notecontent+='[08-19 20:30]修复分享的二维码的bug<br>';
-notecontent+='[08-19 18:30]解决了一些已知问题<br>';
 notecontent+='[08-16 16:10]修复播放记录不能自动更新的bug<br>[08-13 23:30]新增资源库(天空云)<br>';
+notecontent+='</div>';
 
 var SharpUrl_dl='http://app.kkdy.website/';
 var SharpUrl_reg='http://app.kkdy.website/#page_reg';
@@ -28,35 +44,36 @@ var Invitation_code_prefix=9900; //邀请码前缀 ★
 
 var app_name={'web':'看看电影.com','local':'测试_看看电影','bak':'看看电影(备用)'};
 var app_var='<br>中文网址：看看电影.com<br>';
-app_var+='云版号：ver.0819.7.51<br>';
+app_var+='云版号：ver.'+laset_ver+'<br>';
 app_var+=window.location.host;
  //模板文件地址 css js地址 api
- var cdn='https://cdn.jsdelivr.net/gh/kkdysite/webapp@master/tpl/0819.7.51';
- var tpl_Js_html={
+var cdn='https://cdn.jsdelivr.net/gh/kkdysite/app.kkdy.website@master/'+laset_ver;
+var tpl_Js_html={
 	 'web':cdn+'/tpl_Js_html/',
 	 'local':'./tpl_Js_html/',
 	 'bak':"./tpl_Js_html/"
 };
- var tpl_css={
+var tpl_css={
 	 'web':cdn+'/tpl_css/',
 	 'local':'./tpl_css/',
 	 'bak':"./tpl_css/"
 	};
- var tpl_Js_ac={
+var tpl_Js_ac={
 	 'web':cdn+'/tpl_Js_ac/',
 	 'local':'./tpl_Js_ac/',
 	 'bak':"./tpl_Js_ac/"
 	};
-
- var api_vhost={
-	 'web':'http://dl.kkdy.site/_api_vhost_php_sqlite_202008008/api_vhost.php',
-	 'local':'http://192.168.1.88:82/_api_vhost_php_sqlite_202008008/api_vhost.php'
+/** 测试站  区分本地测试 还是服务器  */
+var api_vhost={
+	 'web':'http://dl.kkdy.site/_api_vhost_php_sqlite_'+laset_ver+'/api_vhost.php',
+	 'local':'http://TestUrl/_api_vhost_php_sqlite_'+laset_ver+'/api_vhost.php'
 	};
-
- var localStor_name_u_info='u_info_2020';
- var localStor_name_u_fav='u_fav_2020';
- var localStor_name_u_played='played';
- var localStor_name_u_portable_fav='u_portable_fav2020';
+	//本地测试情况下 把域名里面的 html替换为vhost
+	api_vhost['local']=api_vhost['local'].replace(/TestUrl/,window.location.host.replace(/html/,'vhost'));
+var localStor_name_u_info='u_info_2020';
+var localStor_name_u_fav='u_fav_2020';
+var localStor_name_u_played='played';
+var localStor_name_u_portable_fav='u_portable_fav2020';
 
 //var static_file_suffix ="";
 var portable_url='t.kkdy.website';
@@ -95,6 +112,17 @@ var vip_setting = {
 	"3650000": 50000
 };
 
+
+var webapp_tips=new Array(
+	'偶尔接在失败重新点一下对应的剧集即可，或者切换播放源或解析线路后再播放',
+	'播放过的影片点底部的小时钟或功能导航的播放记录可以看到，也会记住你看到那一集，不用每次搜索',
+	'苹果设备请允许屏幕旋转才可以全屏播放',
+	'部分安卓手机无法投屏，请使用手机自带投屏或者使用机顶盒版',
+	'限于平台制约，我暂时无法记录你看到每一集的第几分钟',
+	'如果喜欢我，请推荐给您的朋友'
+	);
+var hotKeyWords='重启之极海听雷|漂亮书生|大侠霍元甲|他其实没有那么爱你|穿盔甲的少女|非常目击|航海王|幸福还会来敲门|忘不了餐厅|且听凤鸣|极限挑战宝藏行|摩天大楼|月光恋|潮玩人类在哪里|青春环游记|完美夏天|乐队的夏天';
+hotKeyWords+='|上海王|如此可爱的我们|香肠派对|花木兰之大漠营救|连环追击|怪兽猎人|哥斯拉2|狙击手|怪物先生|大幻术师|征途|哪吒之魔童降世|铁血神探';
 
 
 
