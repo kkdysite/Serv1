f184604899f9356a674e8dcf039853c5e9326c3a
kkdysite <68997453+kkdysite@users.noreply.github.com>
2020-08-26 17:57:03 +0800

    Serv1

--- tpl_Js_ac/_initialization.js
+++ tpl_Js_ac/_initialization.js
@@ -34,7 +34,7 @@ var app_var='<br>中文网址：看看电影.com<br>';
 app_var+='云版号：ver.'+laset_ver+'<br>';
 app_var+=window.location.host;
  //模板文件地址 css js地址 api
-var cdn='https://cdn.jsdelivr.net/gh/kkdysite/app.kkdy.website@master/';
+var cdn='https://cdn.jsdelivr.net/gh/kkdysite/Serv1@master/';
 var tpl_Js_html={
 	 'web':cdn+'tpl_Js_html/',
 	 'local':'./tpl_Js_html/',
